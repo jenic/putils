5f7f371ccc7baf2d6af9e7772df659e70ae12407
Jenic Rycr <jenic@wubwub.me>
2013-02-03 05:56:26 -0700

    Weaboo CRC can now handle multiple files at once

--- weaboo-crc.pl
+++ weaboo-crc.pl
@@ -7,9 +7,17 @@ use Compress::Raw::Zlib qw(crc32);
 binmode STDIN;
 
 my $crc = 0;
-my $file = shift || <STDIN>;
-open FH, $file or die "Failed to open file: $!\n";
-$crc = crc32($_, $crc) while (<FH>);
-close FH;
+for my $file ( @ARGV ) {
+	unless (-e $file) {
+		warn "$file not found, skipping", "\n";
+		next;
+	}
+	warn "$file:", "\n";
+	open FH, $file or die "Failed to open file: $!\n";
+	$crc = crc32($_, $crc) while (<FH>);
+	close FH;
 
-printf "%.8X\n", $crc;
+	printf "%.8X\n", $crc;
+} continue {
+	$crc = 0;
+}
